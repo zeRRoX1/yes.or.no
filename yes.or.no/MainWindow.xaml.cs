5f1a951974efd77a6d27e4143ae3029c5ca99a77
makskyz1337@gmail.com <makskyz1337@gmail.com>
2023-10-17 21:02:55 +0300

    Добавьте файлы проекта.

---
+++ yes.or.no/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace yes.or.no
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Yes(object sender, MouseEventArgs e)
+        {
+            if (yes.Content.ToString() == "No")
+            {
+                yes.Content = "Yes";
+                no.Content = "No";
+            }
+        }
+        private void Button_No(object sender, RoutedEventArgs e)
+        {
+            if (no.Content.ToString() == "No")
+            {
+                yes.Content = "No";
+                no.Content = "Yes";
+            }
+        }
+        private void Button_Yes1(object sender, RoutedEventArgs e)
+        {
+            Window1 Window1 = new Window1();
+            Window1.Show();
+        }
+    }
+}
---
+++ yes.or.no/Window2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace yes.or.no
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        private void Button_Yes(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+        private void Button_No(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+    }
+}
